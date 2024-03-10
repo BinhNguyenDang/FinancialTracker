ef6b3a3e4f17c28af2b475d6ced47476fe56aa0b
Binh Nguyen Dang <binhnguyendang2003@gmail.com>
2024-03-10 00:07:03 -0500

    Handling Input Error from Search Function

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,22 +1,26 @@
 class Stock < ApplicationRecord
     def self.new_lookup(ticker_symbol)
-      price_data = Polygon.get_closing_price(ticker_symbol)
-      name = Polygon.get_company_name(ticker_symbol)
+      begin
+        price_data = Polygon.get_closing_price(ticker_symbol)
+        name = Polygon.get_company_name(ticker_symbol)
   
-      if price_data
-        new(
-          ticker: ticker_symbol,
-          name: name,
-          last_price: price_data[:closing_price],
-          high_price: price_data[:high_price],
-          low_price: price_data[:low_price],
-          opening_price: price_data[:opening_price],
-          volume: price_data[:volume],
-          vwap: price_data[:vwap]
-        )
-      else
+        if price_data
+          new(
+            ticker: ticker_symbol,
+            name: name,
+            last_price: price_data[:closing_price],
+            high_price: price_data[:high_price],
+            low_price: price_data[:low_price],
+            opening_price: price_data[:opening_price],
+            volume: price_data[:volume],
+            vwap: price_data[:vwap]
+          )
+        else
+          nil
+        end
+      rescue => e
+        puts "An error occurred while retrieving stock data: #{e.message}"
         nil
       end
     end
   end
-  
