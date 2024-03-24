b9e24dddce5f917d6b86f82b36f28ef283f1a0b1
Binh Nguyen Dang <binhnguyendang2003@gmail.com>
2024-03-24 14:47:55 -0400

    Searching User Function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,26 @@ class UsersController < ApplicationController
   def index
     @friends = current_user.friends
   end
+
+  def search
+    term = params[:users]
+    if term.present?
+      @friend = User.where(email: term).or(User.where(first_name: term)).first
+      if @friend
+        respond_to do |format|
+          format.turbo_stream { render turbo_stream: turbo_stream.replace('user', partial: 'users/user_result', locals: { friend: @friend }) }
+        end
+      else
+        flash[:alert] = 'No users found for the given term (First Initial Capital)'
+        redirect_to following_path
+      end
+    else
+      flash[:alert] = 'Please enter a firstname or email'
+      redirect_to following_path
+    end
+  end
+
+
 end
+  
+
